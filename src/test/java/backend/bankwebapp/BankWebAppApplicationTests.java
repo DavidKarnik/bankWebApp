4dfa32c2207415eb9e4852a2d6ecb85db3724733
David_Karnik <david.karnik@tul.cz>
2023-04-20 11:08:19 +0200

    Init project

---
+++ src/test/java/backend/bankwebapp/BankWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.bankwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
