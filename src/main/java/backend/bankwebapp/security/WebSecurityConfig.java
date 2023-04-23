2821bb64a61aee494b7428bc8c6e575db60e522e
David_Karnik <david.karnik@tul.cz>
2023-04-23 13:11:57 +0200

    Edit html pages v1

--- src/main/java/backend/bankwebapp/security/WebSecurityConfig.java
+++ src/main/java/backend/bankwebapp/security/WebSecurityConfig.java
@@ -36,7 +36,7 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .requestMatchers("/list_users","/try", "/singleAccount").authenticated()
+                .requestMatchers("/list_users","/try", "/singleAccount","**/*.css").authenticated()
                 .anyRequest().permitAll()
                 .and()
                 .formLogin()
