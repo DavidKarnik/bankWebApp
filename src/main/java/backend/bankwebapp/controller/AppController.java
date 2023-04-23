a29e331e54abe5218e4fcfa0ab572ceadc011548
David_Karnik <david.karnik@tul.cz>
2023-04-23 21:18:02 +0200

    Try Deposit button POST - Success

--- src/main/java/backend/bankwebapp/controller/AppController.java
+++ src/main/java/backend/bankwebapp/controller/AppController.java
@@ -3,6 +3,7 @@ package backend.bankwebapp.controller;
 
 import backend.bankwebapp.model.User;
 import backend.bankwebapp.model.UserRepository;
+import backend.bankwebapp.service.AppService;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -66,33 +67,84 @@ public class AppController {
     // FORM
     //
 
+    @GetMapping("/myForm")
+    public String myForm(Model model, Authentication authentication) {
+        String email = authentication.getName();
+        User user = UserRepository.findByEmail(email);
+        model.addAttribute("user", user);
+//        model.addAttribute("success",false);
+//        model.addAttribute("message","aaa");
+        model.addAttribute("show", false);
+        return "myForm";
+    }
+
+    @GetMapping("/deposit")
+    public String myFormDeposit(Model model, Authentication authentication) {
+        String email = authentication.getName();
+        User user = UserRepository.findByEmail(email);
+        model.addAttribute("user", user);
+//        model.addAttribute("success",false);
+//        model.addAttribute("message","aaa");
+        model.addAttribute("show", true);
+        model.addAttribute("success", true);
+        model.addAttribute("message", "/deposit");
+//        model.addAttribute("show", false);
+        return "myForm";
+    }
+
     @PostMapping("/deposit")
-    public String handleDeposit(@RequestParam String accountType, @RequestParam int amount, Model model) {
+    public String handleDeposit(@RequestParam("accountType") String accountType, @RequestParam("amount") int amount, Model model, Authentication authentication) {
         boolean success = true;
         String message = "Deposit successful!";
 
-        // Perform the deposit action here
-        // ...
+        System.out.println("handleDeposit() - success");
 
-        if (!success) {
-            // Set the error message and flag
-            message = "Deposit failed!";
+        //
+        String email = authentication.getName();
+        //
+
+        // Perform the deposit action
+//        String email = authentication.getName();
+        try {
+            // if accountType NOT exist on user account accounts
+            if(!AppService.hasTheAccountOfType(email, accountType)) {
+                AppService.writeToJsonFile(email, accountType, Integer.toString(amount));
+            } else {
+                success = false;
+                message = "Deposit failed! - User already has money account of given type (" + accountType + ")";
+            }
+        } catch (IOException e) {
+//            throw new RuntimeException(e);
+            System.out.println("Error: " + e);
             success = false;
         }
 
-        // Save the accountType and amount parameters in an array
-        Object[] depositParams = { accountType, amount };
+//        if (!success) {
+//            // Set the error message and flag
+//            message = "Deposit failed!";
+//            success = false;
+//        }
 
         // Add the depositParams array to the model for display in the view
-        model.addAttribute("depositParams", depositParams);
+//        model.addAttribute("depositParams", depositParams);
 
         // Add the success or error message to the model
+        User user = UserRepository.findByEmail(email);
+        model.addAttribute("user", user); // for the html page variables !
+
+        model.addAttribute("show", true);
         model.addAttribute("success", success);
         model.addAttribute("message", message);
 
         // Return the name of the view to render
         // return the SAME html page !
         return "myForm";
+    }
+
+    @PostMapping("/deposit2")
+    public String handleDeposit2(@RequestParam("accountType") String accountType, @RequestParam("amount") int amount, Model model, Authentication authentication) {
+        model.addAttribute("message", "ahoj");
+        return "blank";
     }
 
 }
