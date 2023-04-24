d9fdd23c3a4b003a400cf6809c75380a12174f2f
David_Karnik <david.karnik@tul.cz>
2023-04-24 11:39:10 +0200

    Add AppService.addMoneyToAccount()

--- src/main/java/backend/bankwebapp/controller/AppController.java
+++ src/main/java/backend/bankwebapp/controller/AppController.java
@@ -7,8 +7,6 @@ import backend.bankwebapp.service.AppService;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -17,6 +15,8 @@ import java.util.List;
 @Controller
 //@RequestMapping("/api") // prefix need to add to the WebSecurityConfig.java
 public class AppController {
+
+    Boolean successAction = true;
 
     @GetMapping("")
     public String viewHomePage() {
@@ -49,21 +49,6 @@ public class AppController {
     }
 
     //
-    //
-    //
-//    @RequestMapping(value = "/addEmployee", method = RequestMethod.POST, params = "submit")
-//    public String submit(@ModelAttribute("user") final User user,
-//                         final BindingResult result, final ModelMap model) {
-//        // same code as before
-//    }
-//    @RequestMapping(value = "/addEmployee", method = RequestMethod.POST, params = "cancel")
-//    public void cancel(@ModelAttribute("user") final User  user,
-//                       final BindingResult result, final ModelMap model) {
-//        model.addAttribute("message", "You clicked cancel, please re-enter user details:");
-////        return "employeeHome";
-//    }
-
-    //
     // FORM
     //
 
@@ -78,7 +63,18 @@ public class AppController {
         return "myForm";
     }
 
-    @GetMapping("/deposit")
+
+    //
+    // DEPOSIT --------------------------------------------------------------------------------------
+    //
+
+    /**
+     * Called after any form button handler is used -> like refresh of page
+     * @param model - model for html page variables
+     * @param authentication - for find user
+     * @return - return same page - myForm
+     */
+    @RequestMapping(value={"deposit", "payment", "/open", "/close"})
     public String myFormDeposit(Model model, Authentication authentication) {
         String email = authentication.getName();
         User user = UserRepository.findByEmail(email);
@@ -86,54 +82,38 @@ public class AppController {
 //        model.addAttribute("success",false);
 //        model.addAttribute("message","aaa");
         model.addAttribute("show", true);
-        model.addAttribute("success", true);
+        model.addAttribute("success", successAction);
         model.addAttribute("message", "/deposit");
-//        model.addAttribute("show", false);
         return "myForm";
     }
 
     @PostMapping("/deposit")
     public String handleDeposit(@RequestParam("accountType") String accountType, @RequestParam("amount") int amount, Model model, Authentication authentication) {
-        boolean success = true;
         String message = "Deposit successful!";
-
-        System.out.println("handleDeposit() - success");
-
-        //
         String email = authentication.getName();
-        //
 
-        // Perform the deposit action
-//        String email = authentication.getName();
+        // Perform the open action
         try {
-            // if accountType NOT exist on user account accounts
-            if(!AppService.hasTheAccountOfType(email, accountType)) {
+            // if accountType exist on user account
+            if(AppService.hasTheAccountOfType(email, accountType)) {
+                // add amount of finance to the account of accountType
                 AppService.writeToJsonFile(email, accountType, Integer.toString(amount));
             } else {
-                success = false;
-                message = "Deposit failed! - User already has money account of given type (" + accountType + ")";
+                successAction = false;
+                message = "Deposit failed! - Account " + accountType + " does not exist";
             }
         } catch (IOException e) {
-//            throw new RuntimeException(e);
             System.out.println("Error: " + e);
-            success = false;
+            successAction = false;
+//            throw new RuntimeException(e);
         }
-
-//        if (!success) {
-//            // Set the error message and flag
-//            message = "Deposit failed!";
-//            success = false;
-//        }
-
-        // Add the depositParams array to the model for display in the view
-//        model.addAttribute("depositParams", depositParams);
 
         // Add the success or error message to the model
         User user = UserRepository.findByEmail(email);
         model.addAttribute("user", user); // for the html page variables !
 
         model.addAttribute("show", true);
-        model.addAttribute("success", success);
+        model.addAttribute("success", successAction);
         model.addAttribute("message", message);
 
         // Return the name of the view to render
@@ -141,10 +121,62 @@ public class AppController {
         return "myForm";
     }
 
-    @PostMapping("/deposit2")
-    public String handleDeposit2(@RequestParam("accountType") String accountType, @RequestParam("amount") int amount, Model model, Authentication authentication) {
-        model.addAttribute("message", "ahoj");
-        return "blank";
+
+    //
+    // OPEN ------------------------------------------------------------------------------------------
+    //
+
+//    /**
+//     * Called after handleOpen PostMapping
+//     * @param model - model for html page variables
+//     * @param authentication - for find user
+//     * @return - return same page - myForm
+//     */
+//    @GetMapping("/open")
+//    public String myFormOpen(Model model, Authentication authentication) {
+//        String email = authentication.getName();
+//        User user = UserRepository.findByEmail(email);
+//        model.addAttribute("user", user);
+////        model.addAttribute("success",false);
+////        model.addAttribute("message","aaa");
+//        model.addAttribute("show", true);
+//        model.addAttribute("success", successAction);
+//        model.addAttribute("message", "/deposit");
+//        return "myForm";
+//    }
+
+    @PostMapping("/open")
+    public String handleOpen(@RequestParam("accountType") String accountType, @RequestParam("amount") int amount, Model model, Authentication authentication) {
+        String message = "Account opened successfully!";
+        String email = authentication.getName();
+
+        // Perform the open action
+        try {
+            // if accountType NOT exist on user account accounts
+            if(!AppService.hasTheAccountOfType(email, accountType)) {
+                // add account of given accountType with given amount of finance
+                AppService.writeToJsonFile(email, accountType, Integer.toString(amount));
+            } else {
+                successAction = false;
+                message = "Open account failed! - User already has money account of given type (" + accountType + ")";
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e);
+            successAction = false;
+//            throw new RuntimeException(e);
+        }
+
+        // Add the success or error message to the model
+        User user = UserRepository.findByEmail(email);
+        model.addAttribute("user", user); // for the html page variables !
+
+        model.addAttribute("show", true);
+        model.addAttribute("success", successAction);
+        model.addAttribute("message", message);
+
+        // Return the name of the view to render
+        // return the SAME html page !
+        return "myForm";
     }
 
 }
--- src/main/java/backend/bankwebapp/service/AppService.java
+++ src/main/java/backend/bankwebapp/service/AppService.java
@@ -14,6 +14,22 @@ import org.json.JSONObject;
 @Service
 public class AppService {
 
+////    Variables for reduce duplicates
+//
+//    // Read the contents of the log.json file into a string
+//    static String contents;
+//
+//    static {
+//        try {
+//            contents = new String(Files.readAllBytes(Paths.get("src/main/resources/log.json")));
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+//    static JSONObject json = new JSONObject(contents);
+//    static JSONArray users = json.getJSONArray("users");
+
+
     /**
      * Method will write into JSON file in accounts array new value type:amount
      * "accounts":["type:amount"]
@@ -26,8 +42,6 @@ public class AppService {
     public static void writeToJsonFile(String email, String type, String amount) throws IOException {
         // Read the contents of the log.json file into a string
         String contents = new String(Files.readAllBytes(Paths.get("src/main/resources/log.json")));
-        // "src/main/resources/log.json"
-        // "target/classes/log.json"
 
         // Parse the contents into a JSONObject
         JSONObject json = new JSONObject(contents);
@@ -96,16 +110,64 @@ public class AppService {
         return false;
     }
 
+    public static Boolean addMoneyToAccount(String email, String type, int amount) throws IOException {
+        // Read the contents of the log.json file into a string
+        String contents = new String(Files.readAllBytes(Paths.get("src/main/resources/log.json")));
+
+        // Parse the contents into a JSONObject
+        JSONObject json = new JSONObject(contents);
+
+        // Get the "users" array
+        JSONArray users = json.getJSONArray("users");
+
+        // Find the user with the matching email
+        for (int i = 0; i < users.length(); i++) {
+            JSONObject user = users.getJSONObject(i);
+            if (user.getString("email").equals(email)) {
+
+                // Get the "accounts" array for the matching user
+                JSONArray accounts = user.getJSONArray("accounts");
+
+                for (int j = 0; j < accounts.length(); j++) {
+
+                    String account = accounts.getString(j);
+                    String[] accountParts = account.split(":");
+                    String _type = accountParts[0];
+                    int oldAmount = Integer.parseInt(accountParts[1]);
+
+                    if (_type.equals(type)) {
+                        // Update the amount of the matching account
+                        int newAmount = oldAmount + amount;
+                        accounts.put(j, _type + ":" + newAmount);
+
+                        // Write the modified JSON back to the log.json file
+                        FileWriter file = new FileWriter("src/main/resources/log.json");
+                        file.write(json.toString());
+                        file.flush();
+                        file.close();
+                        return true;
+                    }
+                }
+                break;
+            }
+        }
+        return false;
+    }
+
 
     /**
      * Test main class
      */
     public static void main(String[] args) throws IOException {
+        // ctrl + alt + L -> beatify the code ! :)
+
 //        System.out.println("Try writeToJsonFile()");
 //        AppService.writeToJsonFile("user@mail.com", "USD", "1000");
 
-        System.out.println("Try hasTheAccountOfType()");
-        System.out.println(AppService.hasTheAccountOfType("user@mail.com", "USD"));
-        // ctrl + alt + L -> beatify the code ! :)
+//        System.out.println("Try hasTheAccountOfType()");
+//        System.out.println(AppService.hasTheAccountOfType("user@mail.com", "USD"));
+
+        System.out.println("Try addMoneyToAccount()");
+        System.out.println(AppService.addMoneyToAccount("user@mail.com", "USD", 1001));
     }
 }
