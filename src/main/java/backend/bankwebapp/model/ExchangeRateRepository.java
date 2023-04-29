199caf2309b268f9ab986ecf2e7672d1022a1457
David_Karnik <david.karnik@tul.cz>
2023-04-29 19:38:09 +0200

    Add exchangeRate.txt and Scheduler for task

---
+++ src/main/java/backend/bankwebapp/model/ExchangeRateRepository.java
@@ -0,0 +1,8 @@
+package backend.bankwebapp.model;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ExchangeRateRepository {
+    
+}
--- src/main/java/backend/bankwebapp/service/ExchangeRateService.java
+++ src/main/java/backend/bankwebapp/service/ExchangeRateService.java
@@ -1,6 +1,11 @@
 package backend.bankwebapp.service;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import org.apache.hc.client5.http.classic.methods.HttpGet;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
@@ -8,12 +13,59 @@ import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.core5.http.ParseException;
 import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
+@EnableScheduling // for schedule tasks, like refresh everyday or after specific time
 public class ExchangeRateService {
 
-    private static final String ulrCNB = "https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt";
+
+    /**
+     * Method will run and refresh values in exchangeRate.txt file every workday (Monday-Friday) at 15:00 Prague time
+     * (cron = second minute hour day-of-month month-of-the-year *)
+     * The "?" means that the day-of-month field is ignored, and it gets last param "MON-FRI"
+     */
+    @Scheduled(cron = "0 0 15 ? * MON-FRI", zone = "Europe/Prague")
+    public void refreshFileExchangeRate() {
+        String url = "https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt";
+        String htmlContent = getHtmlContent(url);
+
+        try {
+            // Get the current date and time to include in the output file name
+            LocalDateTime now = LocalDateTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            String timestamp = now.format(formatter);
+
+            // Write the htmlContent to a file
+            FileWriter writer = new FileWriter("src/main/resources/exchangeRate.txt", true);
+            writer.write(timestamp + "\n");
+            writer.write(htmlContent + "\n");
+            writer.close();
+        } catch (IOException e) {
+            // Handle the exception
+        }
+    }
+
+    /**
+     * Read exchangeRate.txt file to the String
+     * @return - String content of exchangeRate.txt
+     */
+    public static String readExchangeRateFile() {
+        StringBuilder contentBuilder = new StringBuilder();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/exchangeRate.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                contentBuilder.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            // Handle the exception
+        }
+
+        return contentBuilder.toString();
+    }
 
     /**
      * Load html page content into String
@@ -36,18 +88,17 @@ public class ExchangeRateService {
     }
 
     /**
-     * Return String[][] array from currency list by cnb.cz and cuts the header info
-     *
-     * @param text - Specific html page content Name|Type|Amount|Currency\n...
+     * Return String[][] array from currency list by cnb.cz and cuts the head info
+     * Gets specific html page content Country|Currency|Amount|Code|ExchangeRate\n (e.g. Polsko|zlotý|1|PLN|5,121)
      * @return - Specific String[][] array of exchange currency table
      */
-    public static String[][] doStringArray(String text) {
-        String[] edit = text.split("\n");
-        String[][] output = new String[edit.length - 2][5];
+    public static String[][] getExchangeRateStringArray() {
+        String[] edit = ExchangeRateService.readExchangeRateFile().split("\n");
+        String[][] output = new String[edit.length - 3][5];
         String line;
-        for (int i = 0; i < edit.length - 2; i++) {
-            line = edit[i + 2].replace('\u007c', ' ');
-            output[i] = line.split(" ");
+        for (int i = 0; i < edit.length - 3; i++) {
+            line = edit[i + 3];//.replace('\u007c', ' '); // |
+            output[i] = line.split("\\|");
         }
         return output;
     }
@@ -72,13 +123,14 @@ public class ExchangeRateService {
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!\n");
 
-        String htmlContent = getHtmlContent(ulrCNB);
-        System.out.println(htmlContent);
+//        String htmlContent = getHtmlContent("https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt");
+//        System.out.println(htmlContent);
 
 //        char verticalLine = '\u007c'; // '|'
 //        System.out.println(String.valueOf(verticalLine));
 
         System.out.println("-------------------\n");
-        printArray(doStringArray(htmlContent));
+        printArray(getExchangeRateStringArray());
+
     }
 }
